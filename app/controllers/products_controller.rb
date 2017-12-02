480c071084dccc5818d59b5f800663d7f3d763f3
Edgardo David <edgardo.burgos94@gmail.com>
2017-12-02 14:45:27 -0500

    Add checkbox and category params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,14 +4,35 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new()
+    @categories = Category.all
   end
 
   def create
+    @product = Product.create(product_params)
+    puts("....................los resultados del checkbox son.....................")
+    puts(product_params)
+    puts(category_params)
+
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
   end
+
   def destroy
     product = Product.find(params[:id])
     product.destroy
 
     redirect_to products_path
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :price)
+  end
+  def category_params
+    params.require(:category).permit! if params[:ad].present?
   end
 end

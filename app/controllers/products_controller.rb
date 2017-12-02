1cfa946913c2998ac524fb8ea8b319c012957d5b
Edgardo David <edgardo.burgos94@gmail.com>
2017-12-02 16:54:11 -0500

    Add edit product and check the categories

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -28,6 +28,30 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+    @product = Product.find(params[:id])
+    @categories = Category.all
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      
+      categories = category_params
+      @product.categories.delete(Category.all)
+      redirect_to products_path
+      if categories != nil
+        categories.each do |category|
+          id = category[1..-1].to_i
+          puts("el id es #{id}")
+          Category.find(id).products << @product
+        end
+      end
+    else
+      render :edit
+    end
+  end
+
   def destroy
     product = Product.find(params[:id])
     product.destroy
